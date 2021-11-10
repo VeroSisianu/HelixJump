456a568c07a614370734b64012cac878c6aa7ee0
VeroSisianu <nica220196@gmail.com>
2021-11-10 11:04:37 +0200

    Slider

--- HelixJump/Assets/Scripts/UIManager.cs
+++ HelixJump/Assets/Scripts/UIManager.cs
@@ -7,7 +7,19 @@ public class UIManager : MonoBehaviour
 {
     public Text ScoreText;
     public Text BestScoreText;
+    public Slider PercentsSlider;
+    public Transform FireBall;
+    float _beginningPos;
+    float _finalPos = -15f;
+    float _fullDistanceToFinalGround;
+    float _pastSliderValue = 0;
 
+
+    private void Start()
+    {
+         _beginningPos = FireBall.position.y;
+         _fullDistanceToFinalGround = _beginningPos - _finalPos;
+    }
     void Update()
     {
         if (ScoreText.text != ScoreManager.CurrentScore.ToString())
@@ -18,5 +30,14 @@ public class UIManager : MonoBehaviour
         {
             BestScoreText.text = $"BEST : {ScoreManager.BestScore}";
         }
+
+        var _currentDistanceToFinalGround = FireBall.position.y - _finalPos;
+        var _sliderValue = (_fullDistanceToFinalGround - _currentDistanceToFinalGround) / _fullDistanceToFinalGround * 100;
+        
+        if (_sliderValue <= 100 && _sliderValue >= 0 && _sliderValue > _pastSliderValue)
+        {
+            PercentsSlider.value = _sliderValue;
+        }
+        _pastSliderValue = PercentsSlider.value;
     }
 }
